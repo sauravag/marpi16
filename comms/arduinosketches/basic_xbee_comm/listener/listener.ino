5510c8cdb1f589dd2d28e1e1e0e366c926e94501
Rebecca <sont89@tamu.edu>
2016-10-21 12:12:07 -0500

    Basic arduino xbee tutorial

---
+++ comms/arduinosketches/basic_xbee_comm/listener/listener.ino
@@ -0,0 +1,41 @@
+/*****************************************************************
+This code allows arduino to receive data xBee
+and then stream it to serial.
+
+Set up a software serial port to pass data between an XBee Shield
+and the serial monitor.
+
+Hardware Hookup:
+  The XBee Shield makes all of the connections you'll need
+  between Arduino and XBee. If you have the shield make
+  sure the SWITCH IS IN THE "DLINE" POSITION. That will connect
+  the XBee's DOUT and DIN pins to Arduino pins 2 and 3.
+
+*****************************************************************/
+// We'll use SoftwareSerial to communicate with the XBee:
+#include <SoftwareSerial.h>
+// XBee's DOUT (TX) is connected to pin 2 (Arduino's Software RX)
+// XBee's DIN (RX) is connected to pin 3 (Arduino's Software TX)
+SoftwareSerial XBee(2, 3); // RX, TX
+
+void setup()
+{
+  // Set up both ports at 9600 baud. This value is most important
+  // for the XBee. Make sure the baud rate matches the config
+  // setting of your XBee.
+  XBee.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+ 
+  if (XBee.available())
+  { // If data comes in from XBee, send it out to serial monitor
+    String msg = XBee.readString();
+    msg = "I received: " + msg + "\n";
+    Serial.print(msg);
+  }
+
+}
+
