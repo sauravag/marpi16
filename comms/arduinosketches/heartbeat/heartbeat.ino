2eb811ef6b874dcc34b4aaf1620b677cb589a4dc
sauravag <saurav6@gmail.com>
2016-11-04 11:37:01 -0500

    heartbeat only every 1 second

--- comms/arduinosketches/heartbeat/heartbeat.ino
+++ comms/arduinosketches/heartbeat/heartbeat.ino
@@ -19,25 +19,39 @@ Hardware Hookup:
 // XBee's DIN (RX) is connected to pin 3 (Arduino's Software TX)
 SoftwareSerial XBee(2, 3); // RX, TX
 
+long previousMillis = 0;   // will store last time heartbeat was sent
+ 
+// the follow variables is a long because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long interval = 1000; 
+
 void setup()
 {
   // Set up both ports at 9600 baud. This value is most important
   // for the XBee. Make sure the baud rate matches the config
   // setting of your XBee.
-  XBee.begin(9600);
+  XBee.begin(57600);
   Serial.begin(115200);
 }
 
 void loop()
 {
-
-   XBee.write("robot 1 is alive."); // broadcast heartbeat 
-   
+  // check to see if it's time to send heartbeat; that is, if the 
+  // difference between the current time and last time you sent heartbeat 
+  // is bigger than the interval at which you want
+  unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis > interval) {
+    // save the last time you blinked the LED 
+    previousMillis = currentMillis;   
+ 
+    XBee.write("robot 3 is alive."); // broadcast heartbeat 
+  }
+    
   if (XBee.available())
   { // If data comes in from XBee, send it out to serial monitor
     Serial.write(XBee.read());
   }
   
-    delay(1000); // sleep for 1 second
 }
 
