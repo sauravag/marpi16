bc5235dcd2299a9315d93cfa22649d433e3a90e9
Rebecca <sont89@tamu.edu>
2016-11-18 11:43:35 -0600

    updated compose message

--- comms/arduinosketches/compose_message/compose_message.ino
+++ comms/arduinosketches/compose_message/compose_message.ino
@@ -4,6 +4,12 @@
 // XBee's DIN (RX) is connected to pin 3 (Arduino's Software TX)
 SoftwareSerial XBee(2, 3); // RX, TX
 
+long previousMillis = 0;   // will store last time heartbeat was sent
+ 
+// the follow variables is a long because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long interval = 1000; 
+
 void setup()
 {
   // Set up both ports at 9600 baud. This value is most important
@@ -34,19 +40,33 @@ String compose_message(int robotNumber, int jobNumber, int jobStatus, char healt
 
 void loop(){
   //set values
-  int robotNumber = 2;
-  int jobNumber = 6;
-  int jobStatus = 50;
-  char health = 'A';
-  int batteryLife = 60;
-  float z = 32.0;
-  float theta = 33.4;
+  int robotNumber = 4;
+  int jobNumber = 8;
+  int jobStatus = 70;
+  char health = 'C';
+  int batteryLife = 80;
+  float z = 34.0;
+  float theta = 35.4;
   String k;
   
   k = compose_message(robotNumber, jobNumber, jobStatus, health, batteryLife, z, theta); 
 
+// check to see if it's time to send heartbeat; that is, if the 
+  // difference between the current time and last time you sent heartbeat 
+  // is bigger than the interval at which you want
+  unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis > interval) {
+    // save the last time you blinked the LED 
+    previousMillis = currentMillis;   
   XBee.print(k);
-  Serial.print(k);
-  delay(1000);
+  }
+  
+  if (XBee.available())
+  { // If data comes in from XBee, send it out to serial monitor
+    Serial.write(XBee.read());
+  }
+
+  
 }
 
