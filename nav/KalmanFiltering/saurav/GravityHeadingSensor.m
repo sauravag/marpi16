bcf243d2cdccbc846265a2f54c5e6f5bddb1a1ec
saurav agarwal <saurav6@gmail.com>
2016-12-02 16:34:31 -0600

    Adding kalman filtering matlab code

---
+++ nav/KalmanFiltering/saurav/GravityHeadingSensor.m
@@ -0,0 +1,64 @@
+classdef GravityHeadingSensor < ObservationModelBase
+    properties (Constant) % Note that you cannot change the order of the definition of properties in this class due to its ugly structure!! (due to dependency between properties.)
+        obsDim = 4; % dimension of the observation vector
+        obsNoiseDim = 4; % observation noise dimension. In some other observation models the noise dimension may be different from the observation dimension.
+    end
+    
+    properties
+        zeroNoise = zeros(4,1);
+        R_v = 1e-8*eye(4);
+        accG = 9.8;
+        g_pipe = [-9.8;0;0];
+    end
+    
+    methods
+        
+        function obj = GravityHeadingSensor()
+            obj@ObservationModelBase();
+        end
+        
+        function z = getObservation(obj,x,v)
+            
+            phi = x(1);
+            theta = x(2);                      
+            
+            R_bp = [sin(theta)*sin(phi), -sin(theta)*cos(phi), -cos(theta); ...
+                -cos(theta)*sin(phi), cos(theta)*cos(phi), -sin(theta); ...
+                cos(phi), sin(phi), 0];
+            
+            z = [R_bp'*obj.g_pipe; phi] + v;
+            
+            z(4) = pi_to_pi(z(4));
+        end
+                
+        function H = getObservationJacobian(obj,x)
+            phi = x(1);
+            theta = x(2);
+            g = obj.accG;
+            
+            H = [ -g*sin(theta)*cos(phi), -g * cos(theta)*sin(phi), 0;...
+                -g*sin(theta)*sin(phi), g*cos(theta)*cos(phi), 0;...
+                0, -g*sin(theta), 0 ;...
+                1, 0, 0];
+        end
+        
+        function M = getObservationNoiseJacobian(obj,x)
+            M = eye(4);
+        end
+
+        function v = generateObservationNoise(obj,x)
+            v = mvnrnd(obj.zeroNoise,obj.R_v);
+            v = 0*v';
+        end
+        
+        function innov = computeInnovation(obj,Xprd,Zg)
+            
+            innov = Zg - obj.getObservation(Xprd, obj.zeroNoise);
+            
+            innov(4) = pi_to_pi(innov(4));
+            
+        end
+            
+    end
+    
+end
---
+++ nav/KalmanFiltering/saurav/PancakeRobot.m
@@ -0,0 +1,66 @@
+classdef PancakeRobot < MotionModelBase
+    properties (Constant = true) % note that all properties must be constant, because we have a lot of copies of this object and it can take a lot of memory otherwise.
+        stDim = 3; % state dimension
+        ctDim = 2;  % control vector dimension
+        wDim = 2;   % Process noise (W) dimension      
+        zeroNoise = [0;0]; % zero noise vector       
+    end
+    
+    properties
+         R = 1e-2*60.96/2; % radius of pipe in meters
+    end
+    
+    methods
+        
+        function obj = PancakeRobot()
+            obj@MotionModelBase();
+            obj.dt = 0.1; % delta_t for time discretization
+            obj.Q_w = diag([0.01^2, 0.001^2]); % covariance of process noise
+        end
+        
+        function x_next = evolve(obj, x, u, w)
+            
+            phi = x(1);
+            
+            sqrt_dt = sqrt(obj.dt); 
+            
+            delta_phi =  u(2)*obj.dt + w(2)*sqrt_dt;
+            
+            delta_theta = u(1)*sin(phi)*obj.dt/obj.R +  w(1)*sin(phi)*sqrt_dt/obj.R;
+            
+            delta_d = u(1)*cos(phi)*obj.dt + w(1)*cos(phi)*sqrt_dt;
+            
+            x_next = x + [delta_phi; delta_theta; delta_d];
+            
+            x_next(1) = pi_to_pi(x_next(1));
+            x_next(2) = pi_to_pi(x_next(2));
+            
+        end
+        
+        function F = getStateTransitionJacobian(obj, x, u)
+        
+                phi = x(1);
+                
+                F = [1, 0, 0;...
+                    u(1)*cos(phi)*obj.dt/obj.R, 1, 0;...
+                    -u(1)*sin(phi)*obj.dt, 0, 1];
+                    
+        end
+                
+        function L = getProcessNoiseJacobian(obj, x) % noise Jacobian
+            phi = x(1);
+            
+            sqrt_dt = sqrt(obj.dt);
+            
+            L = [0, sqrt_dt;...
+                sin(phi)*sqrt_dt/obj.R, 0;...
+                cos(phi)*sqrt_dt, 0];
+        
+        end
+                
+        function w = generateProcessNoise(obj, x, u) % simulate (generate) process noise based on the current poistion and controls
+            w = mvnrnd(obj.zeroNoise,obj.Q_w);
+        end
+        
+    end
+end
